e12179552a2afc6fbcab9bb08406be240e936f68
JongDo-android <whdgus090@gmail.com>
2022-05-17 17:00:32 +0900

    feat(common): CoronaMapApplication HiltAndroidApp 설정

---
+++ app/src/main/java/android/autocrypt/coronamap/common/CoronaMapApplication.kt
@@ -0,0 +1,9 @@
+package android.autocrypt.coronamap.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CoronaMapApplication: Application() {
+
+}
