96bd894f879909d8fe56be0016f113500f35b182
JongDo-android <whdgus090@gmail.com>
2022-05-18 11:44:56 +0900

    feat(ui): SplashActivity data binding 추가

--- app/src/main/java/android/autocrypt/coronamap/ui/SplashActivity.kt
+++ app/src/main/java/android/autocrypt/coronamap/ui/SplashActivity.kt
@@ -1,12 +1,24 @@
 package android.autocrypt.coronamap.ui
 
+import android.annotation.SuppressLint
 import android.autocrypt.coronamap.R
+import android.autocrypt.coronamap.databinding.ActivitySplashBinding
+import android.autocrypt.coronamap.viewmodel.SplashViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import dagger.hilt.android.AndroidEntryPoint
 
+@SuppressLint("CustomSplashScreen")
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    private val splashViewModel: SplashViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash)
+        binding.lifecycleOwner = this
+        binding.viewModel = splashViewModel
     }
 }
